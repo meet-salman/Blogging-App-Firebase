13aca74f3880ef3ed8d12fd1faf55c09e6e1b08b
Salman Ahmed <say2salmanahmed@gmail.com>
2023-11-30 21:00:43 +0500

    Login & Signup Created

---
+++ script/login.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+
+const loginForm = document.querySelector('#login-form');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
---
+++ script/signup.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+const signupForm = document.querySelector('#signup-form');
+const firstName = document.querySelector('#first-name');
+const lastName = document.querySelector('#last-name');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm-password');
+
+
+
