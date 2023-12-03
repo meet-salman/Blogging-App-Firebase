b04958f59ab787d279e94f131593a2e8c29a0e40
Salman Ahmed <say2salmanahmed@gmail.com>
2023-12-03 13:40:49 +0500

    User profile Pic Upload Work Completed

--- script/home.js
+++ script/home.js
@@ -2,13 +2,13 @@ const goodTime = document.querySelector('#good-time');
 
 
 const rightNow = new Date();
-// const rightNow = new Date('5 Jan 2012, 3:23 PM');
+// const rightNow = new Date('5 Jan 2012, 12:25 AM');
 
 let hours = rightNow.getHours();
 const minutes = rightNow.getMinutes();
 const amPM = hours >= 12 ? 'PM' : 'AM';
 
-console.log(`${hours} : ${minutes} ${amPM}`);
+// console.log(`${hours} : ${minutes} ${amPM}`);
 
 
 if (amPM === 'AM' && hours >= 4 && hours < 12) {
@@ -29,4 +29,3 @@ if (amPM === 'AM' && hours >= 4 && hours < 12) {
     hours = hours % 12;
     goodTime.innerHTML = 'Good Night Readers'
 }
-
